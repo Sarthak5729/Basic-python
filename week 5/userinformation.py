2e2c09eb8c15e118f034b18be33a1395d2221268
Sarthak <161110477+Sarthak5729@users.noreply.github.com>
2024-09-18 15:39:40 +1200

    Add files via upload

---
+++ week 5/userinformation.py
@@ -0,0 +1,27 @@
+
+"""
+description :- create a function which create generate unique id  for each user and store user information in a dictionary
+Author Sarthak modi
+
+
+"""
+def  collect_user_information(id_counter):
+    print("Enter User information")
+    name  = input("Name: ")
+    age  = input("Age: ")
+    email  = input("Email: ")
+
+    unique_id = id_counter + 1
+    id_counter = unique_id
+    print("User information")
+    print("User name :", name)
+    print("Useg  age :", age)
+    print("User email :", email)
+    print("User id :",unique_id)
+def main():
+    id_counter = 5000
+    collect_user_information(id_counter)
+main()
+
+
+
